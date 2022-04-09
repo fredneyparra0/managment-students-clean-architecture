4db30a143cd7870fd85c5d5d62ddc2fdd4ad412d
fredney parra <fredneyparra0@gmail.com>
2022-04-09 06:24:30 -0400

    conection to database switch completed

--- src/dataAccess/connectDatabase.ts
+++ src/dataAccess/connectDatabase.ts
@@ -1,17 +1,19 @@
 import { ConnectionMongoDb } from './mongoDb/connection';
 
-async function connectDataBase() {
+
+async function connectDataBase():Promise<void> {
     try {
         if(process.env.DATA_DATABASE === 'MONGO') {
             await ConnectionMongoDb.connection();
             console.log(`Connected sucessfull to database ${process.env.DATA_DATABASE}`);
         }
-        // else {
-        //     // await ConnectionPostgresql.connection();
-        // }
+        else {
+            await ConnectionMongoDb.connection();
+            console.log('connected to database for default: MONGODB');
+        }
     } catch (err) {
         console.log(`ERROR TO CONNECT DataBase ${process.env.DATA_BASE}`);
     }
 }
 
-connectDataBase()
+connectDataBase();
