a0ec941064016ef9b85d1296bea2e10ac887c7e0
fredney parra <fredneyparra0@gmail.com>
2022-04-09 06:27:06 -0400

    type from express

--- src/webServer/index.ts
+++ src/webServer/index.ts
@@ -1,11 +1,12 @@
-import express from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import { router } from './routes/main-routes';
-
 dotenv.config();
-const app = express();
+
+import '../dataAccess/connectDatabase';
+const app:Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api-v1', router);
+app.use('/api/v1', router);
 
 app.listen(process.env.PORT, () => console.log(`server is running in http://localhost/${process.env.PORT}`));
