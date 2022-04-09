b19fdde9507d3442de5ced74283bfecc5ef3a7f6
fredney parra <fredneyparra0@gmail.com>
2022-04-09 06:22:55 -0400

    use case student completed

--- src/useCases/students.ts
+++ src/useCases/students.ts
@@ -1,21 +1,54 @@
 import { IStudent } from "../core/interfaces/student-interface";
+import { RepositoryStudentMongoDb } from "../dataAccess/mongoDb/repositories/student-repository";
+import { Query, Document, ObjectId } from 'mongoose';
+import { IRepositoryStudent } from "../core/repository/student-repository";
+export class UseCaseStudent implements IRepositoryStudent {
 
-export class UseCaseStudent {
+    constructor(private repository: RepositoryStudentMongoDb) {}
+    //TODO: logica de negocio
     
-    findAllStudents(): any {
-        // const dataReturn = [
-        //     {
-        //         name: 'fredney',
-        //         email: 'fredneyparra0@gmail.com',
-        //         password: 'estrella1215',
-        //         dateOfBrithday: new Date,
-        //         age: 22,
-        //         coursingYear: 'primaria',
-        //         countrie: 'venezuela',
-        //         phoneNumber: 4120642890,
-        //         sectionOfStudent: '5t0 A'
-        //     }
-        // ];
-        return 'hola mundo';
+    findAllStudents(params?: any): Promise<IStudent[] | null> {
+        try {
+            const dataRes = this.repository.findAllStudents();
+            return dataRes;
+        } catch (err: unknown) {
+            throw new Error('error in <Error>');
+        }
+    }
+
+    findStudentById(id: string | number | ObjectId): Promise<IStudent | null> {
+        try {
+            const dataRes = this.repository.findStudentById(id);
+            return dataRes;
+        } catch (err: unknown) {
+            throw new Error('error in <Error>');
+        }
+    }
+
+    createStudent(data: IStudent): Promise<IStudent | null> {
+        try {
+            const dataRes = this.repository.createStudent(data);
+            return dataRes;
+        } catch (err: unknown) {
+            throw new Error('error in <Error>');
+        }
+    }
+
+    updateStudent(idStudent: string | number | ObjectId,data: IStudent): Promise<IStudent | null> {
+        try {
+            const dataRes = this.repository.updateStudent(idStudent, data);
+            return dataRes;
+        } catch (err: unknown) {
+            throw new Error('error in <Error>');
+        }
+    }
+
+    deleteStudent(idFromDelete: string | number | ObjectId): Promise<IStudent | null> {
+        try {
+            const dataRes = this.repository.deleteStudent(idFromDelete);
+            return dataRes;
+        } catch (err: unknown) {
+            throw new Error('error in <Error>');
+        }
     }
 }
